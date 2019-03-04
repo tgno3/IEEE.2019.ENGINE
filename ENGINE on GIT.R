ebf09cb356eb9f8c59c2700e48bf10a8c65a284e
tgno3 <tgno3.com@gmail.com>
2019-03-04 23:47:18 +0900

    Table 2~4 added

--- ENGINE on GIT.R
+++ ENGINE on GIT.R
@@ -1,6 +1,6 @@
 #########################################################################################################################
 ### Project  : Technology assessment of Engines: TSC vs NA
-### Script   : Engine.R
+### Script   : Engine on GIT.R
 ### Contents : IEEE Access paper
 #########################################################################################################################
 
@@ -26,38 +26,56 @@ wd   <- matrix(c(0, 0, 1), 1)
 sg   <- "min"
 
 
-c <- matrix(as.numeric(dataset.engine.2015[,3]), ncol = 1)
-n <- matrix(c(dataset.engine.2015[,1]),ncol=1)
-p <- matrix(c(dataset.engine.2015[,8]),ncol=1)
-x <- matrix(as.numeric(dataset.engine.2015[,4]),ncol=1)
-y <- matrix(as.numeric(dataset.engine.2015[,5:7]),ncol=3)
-d <- matrix(as.numeric(dataset.engine.2015[,2]),ncol=1)
-g <- matrix(c(rep(0,nrow(x)),y),nrow=nrow(x),ncol=4)
-
-
-## subset
-x_4 <- subset(x, c==4);y_4<-subset(y,c==4);d_4<-subset(d,c==4);g_4<-subset(g,c==4);n_4<-subset(n,c==4)
-x_6 <- subset(x, c==6);y_6<-subset(y,c==6);d_6<-subset(d,c==6);g_6<-subset(g,c==6);n_6<-subset(n,c==6)
-x_8 <- subset(x, c==8);y_8<-subset(y,c==8);d_8<-subset(d,c==8);g_8<-subset(g,c==8);n_8<-subset(n,c==8)
-
-
 #########################################################################################################################
 ### Analysis
 #########################################################################################################################
 
 ## Table 1. Descriptive statistics
-table.1 <- cbind(sapply(df[df$Cylinder == 4, c(id.x, id.y)], function(x) c(Min  = min(x),  Med = median(x), 
-                                                                           Mean = mean(x), Max = max(x), Std = sd(x))),
-                 sapply(df[df$Cylinder == 6, c(id.x, id.y)], function(x) c(Min  = min(x),  Med = median(x), 
-                                                                           Mean = mean(x), Max = max(x), Std = sd(x))),
-                 sapply(df[df$Cylinder == 8, c(id.x, id.y)], function(x) c(Min  = min(x),  Med = median(x), 
-                                                                           Mean = mean(x), Max = max(x), Std = sd(x))))
-data.frame(Cylinders  = rep(c(4, 6, 8), each = 4), round(t(table.1), 1))
+descs <- function(x){
+  res <- c(Min  = min(x),  Med = median(x), Mean = mean(x), Max = max(x), Std = sd(x))
+  return(res)
+}
+table.1 <- cbind(sapply(df[df$Cylinder == 4, c(id.x, id.y)], descs),
+                 sapply(df[df$Cylinder == 6, c(id.x, id.y)], descs),
+                 sapply(df[df$Cylinder == 8, c(id.x, id.y)], descs))
+cbind(Cylinders  = rep(c(4, 6, 8), each = 4), round(t(table.1), 1))
 
 
-## Table 2. SOA maps
-table.2 <- map.soa.sf(df[df$Cylinder == 4, id.x], df[df$Cylinder == 4, id.y], df[df$Cylinder == 4, id.t], 
-                      rts, dv[df$Cylinder == 4,], wd, mk = "eff")
-round(table.2, 4)
+## Table 2. SOA maps for 4
+n.cyl  <- 4
+res.ec <- map.soa.sf(df[df$Cylinder == n.cyl, id.x], df[df$Cylinder == n.cyl, id.y], 
+                     df[df$Cylinder == n.cyl, id.t], rts, dv[df$Cylinder == n.cyl,], wd, mk = "eff")
+id.soa <- as.numeric(rownames(df[df$Cylinder == n.cyl, ])[as.numeric(rownames(res.ec))])
+data.frame(Model = as.character(df$Name[id.soa]), 
+           MY    = df$MY[id.soa],
+           Type  = as.character(df$Type[id.soa]), 
+           round(res.ec, 4))
 
-# (work in progress)
+
+## Table 3. SOA maps for 6
+n.cyl  <- 6
+res.ec <- map.soa.sf(df[df$Cylinder == n.cyl, id.x], df[df$Cylinder == n.cyl, id.y], 
+                     df[df$Cylinder == n.cyl, id.t], rts, dv[df$Cylinder == n.cyl,], wd, mk = "eff")
+id.soa <- as.numeric(rownames(df[df$Cylinder == n.cyl, ])[as.numeric(rownames(res.ec))])
+data.frame(Model = as.character(df$Name[id.soa]), 
+           MY    = df$MY[id.soa],
+           Type  = as.character(df$Type[id.soa]), 
+           round(res.ec, 4))
+
+
+## Table 4. SOA maps for 8
+n.cyl  <- 8
+res.ec <- map.soa.sf(df[df$Cylinder == n.cyl, id.x], df[df$Cylinder == n.cyl, id.y], 
+                     df[df$Cylinder == n.cyl, id.t], rts, dv[df$Cylinder == n.cyl,], wd, mk = "eff")
+id.soa <- as.numeric(rownames(df[df$Cylinder == n.cyl, ])[as.numeric(rownames(res.ec))])
+data.frame(Model = as.character(df$Name[id.soa]), 
+           MY    = df$MY[id.soa],
+           Type  = as.character(df$Type[id.soa]), 
+           round(res.ec, 4))
+
+
+
+
+
+
+
